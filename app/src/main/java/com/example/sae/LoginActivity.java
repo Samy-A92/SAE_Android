a7dfbe5169bb41acc519dcf498d0a9a6effe0e62
N1co-LB <contact.nicolaslebreton@gmail.com>
2026-04-08 16:42:20 +0200

    Ajout connexion et session

---
+++ app/src/main/java/com/example/sae/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.example.sae;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        EditText editTextLogin = findViewById(R.id.editTextLogin);
+
+        EditText editTextPassword = findViewById(R.id.editTextPassword);
+
+        TextView textViewErrorConnecting = findViewById(R.id.textViewErrorConnecting);
+        textViewErrorConnecting.setVisibility(View.INVISIBLE);
+        Button buttonConnect = findViewById(R.id.connect);
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String login = editTextLogin.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if(login.equals("tech") && password.equals("tech")) {
+                    SharedPreferences sharedPreferences = getSharedPreferences("Session", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("Connected", true);
+                    editor.apply();
+                    Intent intent = new Intent(LoginActivity.this, TechInventoryActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    textViewErrorConnecting.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/sae/MainActivity.java
+++ app/src/main/java/com/example/sae/MainActivity.java
@@ -1,6 +1,15 @@
 package com.example.sae;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +29,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Session", Context.MODE_PRIVATE);
+        boolean isConnected = sharedPreferences.getBoolean("Connected", false);
+
+        Intent intent;
+
+        if(isConnected){
+            intent = new Intent(MainActivity.this, TechInventoryActivity.class);
+            finish();
+        }
+        else{
+            intent = new Intent(MainActivity.this, LoginActivity.class);
+        }
+
+        startActivity(intent);
+        finish();
+
     }
 }
