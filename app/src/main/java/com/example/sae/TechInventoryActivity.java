eb8878e313ed175c87cd7a5666f28445cb0c2fb9
Samy <68301941+Samy-A92@users.noreply.github.com>
2026-04-08 16:42:28 +0200

    Modification tech pour ajout machine

--- app/src/main/java/com/example/sae/TechInventoryActivity.java
+++ app/src/main/java/com/example/sae/TechInventoryActivity.java
@@ -3,7 +3,8 @@ package com.example.sae;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -18,6 +19,8 @@ import java.util.ArrayList;
 
 public class TechInventoryActivity extends AppCompatActivity {
     private MyAdapter adapter;
+    private RecyclerView recyclerView;
+
     private ArrayList<String> ordinateurs = new ArrayList<>();
     private ArrayList<String> salles = new ArrayList<>();
 
@@ -33,6 +36,7 @@ public class TechInventoryActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         // Liste de 10 noms d'ordinateurs
         ordinateurs.add("PC-Alpha");
         ordinateurs.add("PC-Beta");
@@ -56,14 +60,23 @@ public class TechInventoryActivity extends AppCompatActivity {
         salles.add("Salle 108");
         salles.add("Salle 109");
         salles.add("Salle 110");
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MyAdapter(ordinateurs, salles, position -> {
             afficherDialogueModification(position);
         });
         recyclerView.setAdapter(adapter);
+
+        Button button = findViewById(R.id.ajout_machine_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ajouterDialogueModification();
+            }
+        });
     }
+
     private void afficherDialogueModification(int position) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Modifier la salle pour " + ordinateurs.get(position));
@@ -74,8 +87,36 @@ public class TechInventoryActivity extends AppCompatActivity {
 
         builder.setPositiveButton("Modifier", (dialog, id) -> {
             String nouvelleSalle = input.getText().toString();
-            salles.set(position, nouvelleSalle); // Met à jour la liste
-            adapter.notifyItemChanged(position); // Rafraîchit la ligne dans la vue
+            salles.set(position, nouvelleSalle);
+            adapter.notifyItemChanged(position);
+        });
+
+        builder.setNegativeButton("Annuler", null);
+        builder.show();
+    }
+
+    private void ajouterDialogueModification() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Ajouter une nouvelle machine");
+
+        // On gonfle le layout créé à l'étape 1
+        View view = getLayoutInflater().inflate(R.layout.dialog_add_machine, null);
+        EditText inputNom = view.findViewById(R.id.editNomMachine);
+        EditText inputSalle = view.findViewById(R.id.editSalleMachine);
+        builder.setView(view);
+
+        builder.setPositiveButton("Ajouter", (dialog, id) -> {
+            String nom = inputNom.getText().toString();
+            String salle = inputSalle.getText().toString();
+
+            if (!nom.isEmpty() && !salle.isEmpty()) {
+                ordinateurs.add(nom);
+                salles.add(salle);
+
+                adapter.notifyItemInserted(ordinateurs.size() - 1);
+
+                recyclerView.scrollToPosition(ordinateurs.size() - 1);
+            }
         });
 
         builder.setNegativeButton("Annuler", null);
